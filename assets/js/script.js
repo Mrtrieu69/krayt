d3a0af0cf26f7b843affbbed6d7ff2b52c25ed3c
Mrtrieu69 <trieutam2808@gmail.com>
2023-01-26 23:11:36 +0300

    version1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,38 @@
-const blockLocation = document.querySelector(".location");
+const blocksMenu = document.querySelectorAll(".toggle-menu");
 const locationItems = document.querySelectorAll(".location .item");
 const locationName = document.querySelector(".location .name");
 const btnNext = document.querySelector(".btn.next");
 const btnPrev = document.querySelector(".btn.prev");
 const slider = document.querySelector(".slider .body");
 const dots = document.querySelectorAll(".slider .dot");
+const heartIcons = document.querySelectorAll(".icon-heart");
+const colors = document.querySelectorAll(".color .item");
+const sizes = document.querySelectorAll(".size .size-item");
 
-let isShowBlockLocation = false;
+// let isShowBlockMenu = false;
 let currentSlide = 0;
 
-blockLocation.addEventListener("click", () => {
-    blockLocation.classList.add("active");
-    isShowBlockLocation = true;
+// Handle toggle menu for choice
+blocksMenu.forEach((blockMenu) => {
+    blockMenu.addEventListener("click", () => {
+        if (document.querySelector(".toggle-menu.active")) {
+            document.querySelector(".toggle-menu.active").classList.remove("active");
+        }
+        blockMenu.classList.add("active");
+    });
 });
 
-window.addEventListener("click", (e) => {
-    if (isShowBlockLocation) {
-        if (!e.target.closest(".location")) {
-            blockLocation.classList.remove("active");
-            isShowBlockLocation = false;
+window.addEventListener("click", handleToggleList);
+
+function handleToggleList(e) {
+    if (!e.target.closest(".toggle-menu.active")) {
+        if (document.querySelector(".toggle-menu.active")) {
+            document.querySelector(".toggle-menu.active").classList.remove("active");
         }
     }
-});
+}
 
+// Location
 locationItems.forEach((item) => {
     item.addEventListener("click", (e) => {
         const activeItem = document.querySelector(".location .item.active");
@@ -34,6 +44,7 @@ locationItems.forEach((item) => {
     });
 });
 
+// Slider
 btnPrev.addEventListener("click", () => {
     currentSlide--;
     renderUiSlider();
@@ -72,4 +83,64 @@ function renderUiSlider() {
 
     dots[currentSlide].classList.add("active");
     slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Handle like items in sale
+heartIcons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+        icon.classList.toggle("active");
+    });
+});
+
+// change parameters for product
+colors.forEach((color) => {
+    color.addEventListener("click", () => {
+        document.querySelector(".color .item.active").classList.remove("active");
+        color.classList.add("active");
+    });
+});
+
+sizes.forEach((size) => {
+    size.addEventListener("click", () => {
+        document.querySelector(".size .size-item.active").classList.remove("active");
+        size.classList.add("active");
+    });
+});
+
+// Countdown
+const hourEl = document.querySelector(".time .hour");
+const minuteEl = document.querySelector(".time .minute");
+const secondEl = document.querySelector(".time .second");
+
+const HOUR = 12;
+const MINUTE = 40;
+const SECOND = 20;
+
+let time = HOUR * 3600 + MINUTE * 60 + SECOND;
+
+let timerInterval = setInterval(() => {
+    time--;
+    renderTimer(time);
+    if (time <= 0) {
+        clearInterval(timerInterval);
+    }
+}, 1000);
+
+function renderTimer(time) {
+    const hour = Math.floor(time / 3600);
+    const minute = Math.floor((time - hour * 3600) / 60);
+    const second = time - hour * 3600 - minute * 60;
+
+    hourEl.textContent = zeroPad(hour);
+    minuteEl.textContent = zeroPad(minute);
+    secondEl.textContent = zeroPad(second);
+}
+
+function zeroPad(number) {
+    let numberString = String(number);
+    if (numberString.length < 2) {
+        numberString = "0" + numberString;
+    }
+
+    return numberString;
 }
